7ad5daf6df5e9561b06a5f22b3056e21d9481055
defendimattia <defendi.mattia92@gmail.com>
2025-11-20 15:25:19 +0100

    Add Chips class

---
+++ engine/chips.py
@@ -0,0 +1,15 @@
+class Chips:
+
+    def __init__(self, total=100):
+        self.total = total
+
+    def bet(self, amount):
+        self.total -= amount
+        return amount
+
+    def win(self, amount):
+        self.total += amount * 2
+        return amount * 2
+
+    def can_bet(self, amount):
+        return amount >= self.total
