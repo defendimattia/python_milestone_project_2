bd4ef3b0ca7f6c11bb36b7c274a4b088414eb7f3
defendimattia <defendi.mattia92@gmail.com>
2025-11-20 15:04:40 +0100

    Add need_reset and reset methods in Deck class

--- engine/deck.py
+++ engine/deck.py
@@ -13,4 +13,9 @@ class Deck:
     def deal_card(self, amount=1):
         return tuple(self.all_cards.pop() for _ in range(amount))
 
+    def needs_reset(self, threshold=20):
+        return len(self.all_cards) <= threshold
 
+    def reset(self):
+        self.all_cards = [Card(suit, rank) for suit in suits for rank in ranks]
+        self.shuffle_deck()
