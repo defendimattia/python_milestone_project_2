f4187cd9b95327d9628daef1adf5bd13633bd8fd
defendimattia <defendi.mattia92@gmail.com>
2025-11-20 15:04:18 +0100

    Refactor Deck class initialization

--- engine/deck.py
+++ engine/deck.py
@@ -5,11 +5,7 @@ from random import shuffle
 class Deck:
 
     def __init__(self):
-        self.all_cards = []
-
-        for suit in suits:
-            for rank in ranks:
-                self.all_cards.append(Card(suit, rank))
+        self.all_cards = [Card(suit, rank) for suit in suits for rank in ranks]
 
     def shuffle_deck(self):
         shuffle(self.all_cards)
@@ -17,4 +13,4 @@ class Deck:
     def deal_card(self, amount=1):
         return tuple(self.all_cards.pop() for _ in range(amount))
 
-    
+
