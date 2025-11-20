08ca7913e709867fb765600987248dc2e0dd7ae5
defendimattia <defendi.mattia92@gmail.com>
2025-11-20 14:57:17 +0100

    Add shuffle_deck and deal_card methods in Deck class

--- engine/deck.py
+++ engine/deck.py
@@ -1,4 +1,5 @@
 from card import Card, suits, ranks
+from random import shuffle
 
 
 class Deck:
@@ -9,3 +10,11 @@ class Deck:
         for suit in suits:
             for rank in ranks:
                 self.all_cards.append(Card(suit, rank))
+
+    def shuffle_deck(self):
+        shuffle(self.all_cards)
+
+    def deal_card(self, amount=1):
+        return tuple(self.all_cards.pop() for _ in range(amount))
+
+    
