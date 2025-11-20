d24465655b6a7dec2d6646c6186bb27496202c9f
defendimattia <defendi.mattia92@gmail.com>
2025-11-20 14:39:40 +0100

    Add .gitignore and initial Deck class implementation

---
+++ engine/deck.py
@@ -0,0 +1,11 @@
+from card import Card, suits, ranks
+
+
+class Deck:
+
+    def __init__(self):
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                self.all_cards.append(Card(suit, rank))
