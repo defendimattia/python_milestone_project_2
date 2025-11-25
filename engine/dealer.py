a6fa304afbdd49d373fc15f1f89ca0efa115c50c
defendimattia <defendi.mattia92@gmail.com>
2025-11-25 15:33:34 +0100

    Refactor Dealer play_turn method

--- engine/dealer.py
+++ engine/dealer.py
@@ -9,8 +9,9 @@ class Dealer:
 
     def play_turn(self, deck):
 
-        if self.hand.has_blackjack():
-            return True
-
         while self.hand.value < 17:
             self.hand.add_card(deck.deal_card())
+            if self.hand.bust:
+                return self.hand.value
+
+        return self.hand.value
