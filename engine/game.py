0f50ca8e5573ee12a5cbe10282ecf08bb5d176de
defendimattia <defendi.mattia92@gmail.com>
2025-11-25 11:26:43 +0100

    Add show_welcome_message and clear_screen method to Game class

--- engine/game.py
+++ engine/game.py
@@ -1,3 +1,4 @@
+import os
 from engine.dealer import Dealer
 from engine.deck import Deck
 from engine.player import Player
@@ -10,3 +11,15 @@ class Game:
         self.player = Player()
         self.dealer = Dealer()
 
+    def clear_screen(self):
+        os.system("cls" if os.name == "nt" else "clear")
+
+    def show_welcome_message(self):
+        self.clear_screen()
+        print("🎲 Welcome to Blackjack! 🎲")
+        print("Quick rules:")
+        print("- Goal: reach 21 without going bust")
+        print("- Face cards (J, Q, K) are worth 10, Ace is 1 or 11")
+        print("- Dealer hits until reaching at least 17")
+        print("- You can choose your bet each round")
+        print("Good luck!\n")
--- engine/player.py
+++ engine/player.py
@@ -4,7 +4,7 @@ from hand import Hand
 
 class Player:
 
-    def __init__(self, name, chips_amount=100):
-        self.name = name
+    def __init__(self, chips_amount=100):
+        self.name = "Player"
         self.hand = Hand()
         self.chips = Chips(chips_amount)
